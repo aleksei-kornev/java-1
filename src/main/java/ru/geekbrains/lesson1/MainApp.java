b538fbe6d7fb7ed0bb8afefa1ee07749de964506
kornev <aakor23@gmail.com>
2020-05-27 13:51:26 -0500

    Homework 2.

--- src/main/java/ru/geekbrains/lesson1/MainApp.java
+++ src/main/java/ru/geekbrains/lesson1/MainApp.java
@@ -5,89 +5,137 @@ import jdk.nashorn.internal.parser.JSONParser;
 
 public class MainApp {
     public static void main(String[] args) {
-        //#2 задание - переменные всех пройденных данных
-        int a = 10;
-        float b = 10.1f;
-        boolean c = true;
-        String s = "string";
 
-        //#3 задание
-        System.out.println(Calculation(1,2,4,2));
+        System.out.println("Задание №1");
+        int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                arr[i] = 1;
+            } else {
+                arr[i] = 0;
+            }
+            System.out.print(arr[i]);
+        }
+        System.out.println();
+        System.out.println();
 
-        //#4 задание
-        System.out.println(isZeroToTen(13,5));
+        System.out.println("Задание №2");
+        int[] arr2 = new int[8];
+        for (int i = 0; i < arr2.length; i++) {
+            arr2[i] = i * 3;
+            System.out.print(arr2[i]);
+        }
+        System.out.println();
+        System.out.println();
 
-        //#5 задание
-        isPositive(-5);
+        System.out.println("Задание №3");
+        int[] arr3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < arr3.length; i++) {
+            if (arr3[i] < 6) {
+                arr3[i] *= 2;
+            }
+            System.out.print(arr3[i]);
+        }
+        System.out.println();
+        System.out.println();
 
-        //#6 задание
-        System.out.println(isNegative(-10));
+        System.out.println("Задание №4");
+        int[][] arr4 = new int[10][10];
+        for (int i = 0; i < arr4.length; i++) {
+            arr4[i][i] = 1;
+            arr4[i][arr4.length - i - 1] = 1;
+        }
 
-        //#7 задание
-        printHelloName("Михаил");
+        for (int i = 0; i < arr4.length; i++) {
+            for (int j = 0; j < arr4[i].length; j++) {
+                System.out.print(arr4[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println();
 
-        //№8 задание
-        isLeapYear(2020);
-        isLeapYear(2021);
-        isLeapYear(1900);
-        isLeapYear(2000);
+        System.out.println("Задание №5");
+        int[] arr5 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        int iMax = 0;
+        for (int i = 0; i < arr5.length; i++) {
+            if (arr5[i] > arr5[iMax]) iMax = i;
+        }
+        System.out.println("Максимальный элемент - " + arr5[iMax]);
+        System.out.println();
+
+        System.out.println("Задание №6");
+        System.out.println(checkBalance(new int[]{2, 2, 2, 1, 2, 2, 10, 1}));
+        System.out.println();
+
+        System.out.println("Задание №7");
+        int[] oldArr = {2, 3, 2, 1, 2, 2, 9, 1};
+        int n = 2;
+
+        int[] leftArr = slideArray(oldArr.clone(), -n);
+        int[] rightArr = slideArray(oldArr.clone(), n);
+        System.out.print("Исходный массив: ");
+        for (int i=0;i<oldArr.length;i++){
+            System.out.print(oldArr[i]);
+        }
+        System.out.println();
+        System.out.print("Сдвинули влево на "+n+": ");
+        for (int i=0;i<leftArr.length;i++){
+            System.out.print(leftArr[i]);
+        }
+        System.out.println();
+        System.out.print("Сдвинули вправо на "+n+": ");
+        for (int i=0;i<rightArr.length;i++){
+            System.out.print(rightArr[i]);
+        }
+        System.out.println();
     }
 
-    public static int Calculation (int a, int b, int c, int d) {
-        /*
-        Метод вычисляет выражение a * (b + (c / d)) и возвращающет результат,
-        где a, b, c, d – входные параметры этого метода;
-         */
-        return a * (b + (c / d));
+    public static boolean checkBalance(int[] arr) {
+        for (int i=1; i < arr.length; i++){
+            int leftSum =0;
+            int rightSum = 0;
+            for (int j=0; j<i;j++){
+                //считаем сумму слева
+                leftSum += arr[j];
+            }
+            for (int j=i;j<arr.length;j++){
+                //считаем сумму справа
+                rightSum += arr[j];
+            }
+            if (leftSum == rightSum) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    public static boolean isZeroToTen (int a, int b) {
-        /*
-        Метод, принимающий на вход два числа, и проверяющий что их сумма лежит в пределах от 10 до 20
-        (включительно), если да – вернуть true, в противном случае – false;
-         */
-        return a + b >= 10 && a + b <= 20;
+    public static int[] slideArray(int[] arr, int n) {
+        //Проверяем, не превышает ли смещение размер массива и не задано ли смещение как 0
+        if (Math.abs(n)>arr.length || n==0) return arr;
+
+        //смещаем вправо
+        if (n>0){
+            for (int i=arr.length-1; i>n; i--){
+                arr[i] = arr[i-n];
+            }
+            for (int i=0; i<n; i++){
+                arr[i]=0;
+            }
+            return arr;
+        }
+
+        //смещаем влево
+        if (n<0){
+
+            for (int i=0; i<arr.length+n; i++){
+                arr[i] = arr[i-n];
+            }
+            for (int i=arr.length+n; i<arr.length; i++){
+                arr[i]=0;
+            }
+            return arr;
+        }
+    return arr;
     }
 
-    public static void isPositive (int a){
-        /*
-        Метод, которому в качестве параметра передается целое число, метод печатает в консоль
-        положительное ли число передали, или отрицательное;
-        Замечание: ноль считаем положительным числом.
-         */
-        if (a >= 0) System.out.println(a+" - положительное.");
-        else System.out.println(a+" - оттрицательное.");
-    }
-
-    public static boolean isNegative (int a) {
-        /*
-        Метод, которому в качестве параметра передается целое число,
-        метод должен вернуть true, если число отрицательное;
-         */
-        return a < 0;
-    }
-
-    public static void isLeapYear (int year) {
-        /*
-        метод, который определяет является ли год високосным, и выводит сообщение в консоль.
-        Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
-        1900 - не високосный; 2000 - високосный.
-         */
-        boolean isLeap = false;
-
-        if (year % 4 == 0) isLeap = true;
-        if (year % 100 == 0) isLeap = false;
-        if (year % 400 == 0) isLeap = true;
-
-        if (isLeap) System.out.println(year+" високосный");
-        else System.out.println(year+" не високосный");
-    }
-
-    public static void printHelloName (String name) {
-        /*
-        Метод, которому в качестве параметра передается строка, обозначающая имя,
-        метод должен вывести в консоль сообщение «Привет, указанное_имя!»;
-         */
-        System.out.println("Привет, "+name+"!");
-    }
 }
