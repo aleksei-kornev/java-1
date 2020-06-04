b4661cce1799aa8112547c4b580a12b2efa8410a
kornev <aakor23@gmail.com>
2020-06-04 07:35:32 -0400

    Homework 4. Крестики-нолики.

--- src/main/java/ru/geekbrains/lesson1/MainApp.java
+++ src/main/java/ru/geekbrains/lesson1/MainApp.java
@@ -4,56 +4,128 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class MainApp {
-    public static void main(String[] args) {
+    public static int SIZE = 3;
+    public static int DOTS_TO_WIN = 3;
+    public static final char DOT_EMPTY = '•';
+    public static final char DOT_X = 'X';
+    public static final char DOT_O = 'O';
+    public static char[][] map;
+    public static Scanner sc = new Scanner(System.in);
+    public static Random rand = new Random();
 
-        //Задание №1
-        guessNumberGame();
+    public static void main(String[] args) {
+        initMap();
+        printMap();
+        while (true) {
+            humanTurn();
+            printMap();
+            if (checkWin(DOT_X)) {
+                System.out.println("Победил человек");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+            aiTurn();
+            printMap();
+            if (checkWin(DOT_O)) {
+                System.out.println("Победил Искуственный Интеллект");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("Ничья");
+                break;
+            }
+        }
+        System.out.println("Игра закончена");
     }
 
-    public static void guessNumberGame() {
-        Random rand = new Random();
+    public static boolean checkWin(char symb) {
+        boolean isWinVer = true;    //флаг выигрыша в рядах
+        boolean isWinHor = true;    //флаг выигрыша в строках
+        // Логика: проходим циклом по рядам, и если где-то находим не наш символ - опускаем выигрышный флаг
+        for (int i = 0; i < map.length; i++) {
+            isWinVer = isWinHor = true;
+            for (int j = 0; j < map[0].length; j++) {
+                if (map[i][j] != symb) isWinVer = false;
+                if (map[j][i] != symb) isWinHor = false;
+            }
+            if (isWinVer) return true;
+            if (isWinHor) return true;
+        }
 
-        int attempt = 0;            //кол-во использованных попыток пользователя
-        int maxAttempt = 3;         //максимально позволенное кол-во попыток
-        int userAnswer;        //ответ пользователя
-        boolean playAgain = true;   //пока true - играем еще
+        boolean isWin = true;   //флаг выигрыша в диагоналях
+        for (int i = 0; i < map.length; i++) {
+            if (map[i][i] != symb) isWin = false;
+        }
+        if (isWin) return true;
 
-        Scanner sc = new Scanner(System.in);
+        isWin = true;
+        for (int i = 0; i < map.length; i++) {
+            if (map[i][2 - i] != symb) isWin = false;
+        }
+        if (isWin) return true;
+
+        return false;
+    }
+
+    public static boolean isMapFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY) return false;
+            }
+        }
+        return true;
+    }
+
+    public static void aiTurn() {
+        int x, y;
         do {
-            int number = rand.nextInt(10);
-            System.out.println("Компьютер загадал число от 0 до 9. Попробуйте угадать.");
-            do {
-                System.out.printf("У вас %d попыток.", maxAttempt - attempt);
-                System.out.println("Ваш вариант: ");
-                userAnswer = sc.nextInt();
-                if (userAnswer != number) {
-                    System.out.println("Ответ не верный.");
-                    if (userAnswer > number) {
-                        System.out.println("Ваше число больше загаданного.");
-                    } else {
-                        System.out.println("Ваше число меньше загаданного.");
-                    }
-                    attempt += 1;
-                }
-            } while (userAnswer != number && attempt < maxAttempt);
+            x = rand.nextInt(SIZE);
+            y = rand.nextInt(SIZE);
+        } while (!isCellValid(x, y));
+        System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y + 1));
+        map[y][x] = DOT_O;
+    }
 
-            if (userAnswer == number) {
-                System.out.println("Правильно!");
-            } else {
-                System.out.println("Попытки закончились, вы проиграли. Было загадано число " + number);
+    public static void humanTurn() {
+        int x, y;
+        do {
+            System.out.println("Введите координаты в формате X Y");
+            x = sc.nextInt() - 1;
+            y = sc.nextInt() - 1;
+        } while (!isCellValid(x, y)); // while(isCellValid(x, y) == false)
+        map[y][x] = DOT_X;
+    }
+
+    public static boolean isCellValid(int x, int y) {
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
+        if (map[y][x] == DOT_EMPTY) return true;
+        return false;
+    }
+
+    public static void initMap() {
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j] = DOT_EMPTY;
             }
+        }
+    }
 
-            attempt = 0;
-            System.out.println("Повторить игру еще раз? 1 – да / 0 – нет");
-            int a = sc.nextInt();
-            if (a != 1) {
-                playAgain = false;
+    public static void printMap() {
+        for (int i = 0; i <= SIZE; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(map[i][j] + " ");
             }
-
-        } while (playAgain);
-
-        sc.close();
-        System.out.println("Игра окончена.");
-
+            System.out.println();
+        }
+        System.out.println();
     }
 }
