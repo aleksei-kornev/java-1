e449cebe68110a7fbe3a7c9d89dcd5483392b3be
kornev <aakor23@gmail.com>
2020-06-06 11:46:49 -0400

    Homework for lesson 5

--- src/main/java/ru/geekbrains/lesson1/MainApp.java
+++ src/main/java/ru/geekbrains/lesson1/MainApp.java
@@ -1,131 +1,52 @@
 package ru.geekbrains.lesson1;
 
-import java.util.Random;
-import java.util.Scanner;
-
 public class MainApp {
     public static int SIZE = 3;
-    public static int DOTS_TO_WIN = 3;
-    public static final char DOT_EMPTY = '•';
-    public static final char DOT_X = 'X';
-    public static final char DOT_O = 'O';
-    public static char[][] map;
-    public static Scanner sc = new Scanner(System.in);
-    public static Random rand = new Random();
+    public static Employee[] employeesArray = new Employee[5];
 
     public static void main(String[] args) {
-        initMap();
-        printMap();
-        while (true) {
-            humanTurn();
-            printMap();
-            if (checkWin(DOT_X)) {
-                System.out.println("Победил человек");
-                break;
-            }
-            if (isMapFull()) {
-                System.out.println("Ничья");
-                break;
-            }
-            aiTurn();
-            printMap();
-            if (checkWin(DOT_O)) {
-                System.out.println("Победил Искуственный Интеллект");
-                break;
-            }
-            if (isMapFull()) {
-                System.out.println("Ничья");
-                break;
+        // инициализируем сотрудников
+        employeesArray[0] = new Employee("Smith Garry", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30);
+        employeesArray[1] = new Employee("Black Peter", "CEO", "peter@mailbox.com", "892312345", 60000, 48);
+        employeesArray[2] = new Employee("Cannon John", "CIO", "john@mailbox.com", "892312678", 50000, 47);
+        employeesArray[3] = new Employee("Root Susan", "Engineer", "susan@mailbox.com", "892312386", 30000, 42);
+        employeesArray[4] = new Employee("McFly Katy", "Engineer", "katy@mailbox.com", "892312849", 30000, 30);
+
+        // выводим тех, что удовлетворяют условию
+        for (int i = 0; i < employeesArray.length; i++) {
+            if (employeesArray[i].getAge() > 40) {
+                employeesArray[i].info();
             }
         }
-        System.out.println("Игра закончена");
+
+
     }
 
-    public static boolean checkWin(char symb) {
-        boolean isWinVer = true;    //флаг выигрыша в рядах
-        boolean isWinHor = true;    //флаг выигрыша в строках
-        // Логика: проходим циклом по рядам, и если где-то находим не наш символ - опускаем выигрышный флаг
-        for (int i = 0; i < map.length; i++) {
-            isWinVer = isWinHor = true;
-            for (int j = 0; j < map[0].length; j++) {
-                if (map[i][j] != symb) isWinVer = false;
-                if (map[j][i] != symb) isWinHor = false;
-            }
-            if (isWinVer) return true;
-            if (isWinHor) return true;
-        }
+}
 
-        boolean isWin = true;   //флаг выигрыша в диагоналях
-        for (int i = 0; i < map.length; i++) {
-            if (map[i][i] != symb) isWin = false;
-        }
-        if (isWin) return true;
+class Employee {
+    //ФИО, должность, email, телефон, зарплата, возраст;
+    private String name;
+    private String position;
+    private String email;
+    private String phoneNumber;
+    private int salary;
+    private int age;
 
-        isWin = true;
-        for (int i = 0; i < map.length; i++) {
-            if (map[i][2 - i] != symb) isWin = false;
-        }
-        if (isWin) return true;
-
-        return false;
+    public Employee(String name, String position, String email, String phoneNumber, int salary, int age) {
+        this.age = age;
+        this.email = email;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.position = position;
+        this.salary = salary;
     }
 
-    public static boolean isMapFull() {
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                if (map[i][j] == DOT_EMPTY) return false;
-            }
-        }
-        return true;
+    public void info() {
+        System.out.println("name=" + name + " position=" + position + " email=" + email + " phoneNumber=" + phoneNumber + " salary=" + salary + " age=" + age);
     }
 
-    public static void aiTurn() {
-        int x, y;
-        do {
-            x = rand.nextInt(SIZE);
-            y = rand.nextInt(SIZE);
-        } while (!isCellValid(x, y));
-        System.out.println("Компьютер походил в точку " + (x + 1) + " " + (y + 1));
-        map[y][x] = DOT_O;
-    }
-
-    public static void humanTurn() {
-        int x, y;
-        do {
-            System.out.println("Введите координаты в формате X Y");
-            x = sc.nextInt() - 1;
-            y = sc.nextInt() - 1;
-        } while (!isCellValid(x, y)); // while(isCellValid(x, y) == false)
-        map[y][x] = DOT_X;
-    }
-
-    public static boolean isCellValid(int x, int y) {
-        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
-        if (map[y][x] == DOT_EMPTY) return true;
-        return false;
-    }
-
-    public static void initMap() {
-        map = new char[SIZE][SIZE];
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                map[i][j] = DOT_EMPTY;
-            }
-        }
-    }
-
-    public static void printMap() {
-        for (int i = 0; i <= SIZE; i++) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        for (int i = 0; i < SIZE; i++) {
-            System.out.print((i + 1) + " ");
-            for (int j = 0; j < SIZE; j++) {
-                System.out.print(map[i][j] + " ");
-            }
-            System.out.println();
-        }
-        System.out.println();
+    public int getAge() {
+        return age;
     }
 }
